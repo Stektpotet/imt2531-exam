9ac1bc15c17bb67eed215909f583dd5971ff0bc9
Halvor Smedås <stektpotet@gmail.com>
2018-04-02 10:57:52 +0200

    gl_util.cpp/hpp - Added GLTypeSize() Useful for bufferlayout spec and other features that need the respective GL*-type of a GLenum. might need further updates for adding more descriptive GLenum constants, such as GL_INT_2_10_10_10_REV and GL_BYTE_2_3_3 etc.

---
+++ include/gl_util.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <gl/glew.h>
+#include <iostream>
+
+GLuint GLTypeSize(GLenum type);
---
+++ src/gl_util.cpp
@@ -0,0 +1,23 @@
+#include <gl_util.hpp>
+
+GLuint GLTypeSize(GLenum type)
+{
+switch (type)
+{
+case GL_BOOL:                           return sizeof(GLboolean);
+case GL_BYTE:                           return sizeof(GLbyte);
+case GL_UNSIGNED_BYTE:                  return sizeof(GLubyte);
+case GL_SHORT:                          return sizeof(GLshort);
+case GL_UNSIGNED_SHORT:                 return sizeof(GLushort);
+case GL_INT:                            return sizeof(GLint);
+case GL_INT_2_10_10_10_REV:             return sizeof(GLint);
+case GL_UNSIGNED_INT:                   return sizeof(GLuint);
+case GL_UNSIGNED_INT_2_10_10_10_REV:    return sizeof(GLuint);
+case GL_FLOAT:                          return sizeof(GLfloat);
+case GL_DOUBLE:                         return sizeof(GLdouble);
+default:                
+    printf("WARNING!\nGLenum type: %u not supported!!!", type);
+    return 0;
+}
+}
+
