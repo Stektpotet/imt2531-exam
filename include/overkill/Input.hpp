f74eff88f0c8249fdbbf1c68d717a274e6f156ec
JonShard <jonems@stud.ntnu.no>
2018-04-13 16:51:08 +0200

    Moved input functions into new namespace overkill::Input.

---
+++ include/overkill/Input.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdio.h>
+
+#include <GLFW/glfw3.h>
+
+
+namespace overkill::Input
+{
+    const float aspect = 1;
+    
+    extern float fovy;
+
+    void OnInputKeyPress(GLFWwindow* window, int keyCode, int scanCode, int mods);
+    void OnInputKeyHold(GLFWwindow* window, int keyCode, int scanCode, int mods);
+    void OnInputKeyUnpress(GLFWwindow* window, int keyCode, int scanCode, int mods);
+    void OnInputKey(GLFWwindow* window, int keyCode, int scanCode, int action, int mods);
+    void OnCursorHover(GLFWwindow* window, double x, double y);
+}
