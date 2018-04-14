3fbf9552292d77812f8642ae26e281e0b6a51be0
Jonas Johan Solsvik <jonasjso@stud.ntnu.no>
2018-04-14 20:54:09 +0200

    Parser - new class

--- include/overkill/Parser.hpp
+++ include/overkill/Parser.hpp
@@ -3,19 +3,17 @@
 #include <string>
 #include <string_view>
 
-#include <overkill/Model>
+#include <overkill/Model.hpp>
 
 namespace overkill 
 {
 
-struct KeyInteger  { std::string key; int i;             } 
-struct KeyFloat    { std::string key; float fp;          }
-struct KeyString   { std::string key; std::string str;   }
-struct KeyVertex   { std::string key; Vertex vertex;     }
-struct KeyTriangle { std::string key; Triangle triangle; }
-
-
-
+using Line = std::string;
+struct KeyInteger  { std::string key; int i;             };
+struct KeyFloat    { std::string key; float fp;          };
+struct KeyString   { std::string key; std::string str;   };
+struct KeyVertex   { std::string key; Vertex vertex;     };
+struct KeyTriangle { std::string key; Triangle triangle; };
 
 /// <summary> Translates files into internal datastructure </summary>
 class Parser
@@ -27,9 +25,9 @@ public:
     int startofline =  0;
     int endofline = 9999999;
     int linecount = 0;
-    const string whitelistedCharacters = "0123456789abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ-:";
+    const std::string whitelistedCharacters = "0123456789abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ-:";
 
-    auto nextLine()        -> std::string;
+    auto nextLine()        -> Line;
     auto nextKeyString()   -> KeyString;
     auto nextKeyInteger()  -> KeyInteger;
     auto nextKeyFloat()    -> KeyFloat;
