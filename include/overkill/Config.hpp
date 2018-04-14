0b1e24787288415a8877818827eda87f15f122e2
Jonas Johan Solsvik <jonasjso@stud.ntnu.no>
2018-04-14 10:38:17 +0200

    Config.hpp - fixed warnings

--- include/overkill/Config.hpp
+++ include/overkill/Config.hpp
@@ -11,13 +11,13 @@ namespace overkill::C
     constexpr int VersionMinor = 1;
     constexpr int WinWidth = 800;
     constexpr int WinHeight = 600;
-    constexpr char* WinName = "Assignment 2 - Cube";
+    constexpr char WinName[] = "Assignment 2 - Cube";
     constexpr float FOV = 90.0f;
     constexpr float AspectRatio = (float)WinWidth / (float)WinHeight;
     constexpr float NearClip = 0.1f;
     constexpr float FarClip  = 100.0f;
     const glm::vec4 ClearColor { 1.0f, .8f, .6f, 1.0f};
 
-    constexpr char* PathBaseShader = "assets/shaders/base.shader";
-    constexpr char* PathBaseTexture = "assets/textures/Checkers.jpg";
+    constexpr char PathBaseShader[] = "assets/shaders/base.shader";
+    constexpr char PathBaseTexture[] = "assets/textures/Checkers.jpg";
 }
