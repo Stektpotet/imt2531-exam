6cb29dec61b25c99026c1e1b26badbf9f053617f
Jonas Johan Solsvik <jonasjso@stud.ntnu.no>
2018-04-23 17:06:42 +0200

    moved blender/ to /tools

---
+++ tools/discover.py
@@ -0,0 +1,38 @@
+import sys
+import glob
+import os
+import time
+
+
+def post(tag, extension):
+    print(tag, extension)
+    sys.stdout.flush()
+
+
+def scan_collection(collection_match):
+
+    filenames = glob.glob("./assets/"+collection_match)
+    for fname in filenames:
+
+        fname = os.path.basename(fname)
+        if "." not in fname:
+            continue
+
+        fname = os.path.basename(fname)
+        post(tag        = fname.split(".")[0],
+             extension  = fname.split(".")[1])
+
+
+if __name__ == "__main__":
+
+    running = True
+    while running:
+        print()
+        collection_match = input("Query: ")
+        
+        if collection_match == "EXIT":
+            break
+        
+        scan_collection(collection_match);
+
+    print("EXITING!")
