7d857e8e221e006a89b61e7b127eedc5d3fd4bcf
Jonas Johan Solsvik <jonasjso@stud.ntnu.no>
2018-04-26 17:50:39 +0200

    testing modified pipe

--- tool/test-discover.cpp
+++ tool/test-discover.cpp
@@ -12,9 +12,11 @@
 #ifdef WIN32
 const auto& popen = _popen;
 const auto& pclose = _pclose;
-constexpr char PYTHON_COMMAND[] = "C:\\tools\\Python36\\python.exe tool\\discover.py";
+constexpr char PYTHON_DISCOVER[] = "C:\\tools\\Python36\\python.exe tool\\tool\\discover.py";
+constexpr char PYTHON_MODIFIED[] = "C:\\tools\\Python36\\python.exe tool\\tool\\modified.py";
 #else
-constexpr char PYTHON_COMMAND[] = "/usr/bin/python3 discover.py";
+constexpr char PYTHON_DISCOVER[] = "/usr/bin/python3 tool/discover.py";
+constexpr char PYTHON_MODIFIED[] = "/usr/bin/python3 tool/modified.py";
 #endif
 
 struct FileEvent
@@ -30,13 +32,19 @@ class Watcher
 private:
     static std::vector<FileEvent> events;
 
+    static FILE* modifiedpipe;
+
 public:
     static auto popEvents(std::string eventType, std::string collection)->std::vector<FileEvent>;
-    static void pollEvents();
+    static void discoverFiles();
+    static auto openModified() -> char;
+    static auto pollModified() -> char;
+    static auto closeModified() -> char;
 };
 
 
 std::vector<FileEvent> Watcher::events;
+FILE* Watcher::modifiedpipe;
 
 auto Watcher::popEvents(std::string eventType, std::string collection)->std::vector<FileEvent>
 {
@@ -58,21 +66,23 @@ auto Watcher::popEvents(std::string eventType, std::string collection)->std::vec
     return result;
 }
 
-void Watcher::pollEvents()
+void Watcher::discoverFiles()
 {
     using namespace std::chrono_literals;
 
-    const int DATA_SIZE = 128;
-    char  data[DATA_SIZE] = "\n";
 
-    FILE* pipe = popen(PYTHON_COMMAND, "r");
+
+    FILE* pipe = popen(PYTHON_DISCOVER, "r");
     if (!pipe) {
         printf("FAILED OPENING the pipe\n");
         return;
     }
 
+
+    const int DATA_SIZE = 128;
+    char  data[DATA_SIZE] = "\n";
     char* msg;
-    while (msg = fgets(data, DATA_SIZE, pipe)) {
+    while ((msg = fgets(data, DATA_SIZE, pipe))) {
         printf("msg %s", msg);
 
 
@@ -90,16 +100,74 @@ void Watcher::pollEvents()
     }
     printf("CLOSING the pipe\n");
 }
+
+
+
+auto Watcher::openModified() -> char
+{
+    Watcher::modifiedpipe = popen(PYTHON_MODIFIED, "r");
+    if (!Watcher::modifiedpipe) {
+        printf("FAILED OPENING the pipe\n");
+        return 1;
+    }
+    printf("OPENING the modified pipe\n");
+    return 0;
+}
+
+auto Watcher::pollModified() -> char
+{
+    const int DATA_SIZE = 128;
+    char  data[DATA_SIZE] = "\n";
+
+    char* lastmsg; 
+    char* msg;
+
+    while ((msg = fgets(data, DATA_SIZE, Watcher::modifiedpipe))) {
+        strcpy(lastmsg, msg);
+/*      
+        std::stringstream ss;
+        FileEvent fevent;
+        ss << msg;
+        ss >> fevent.event_t >> fevent.collection >> fevent.tag >> fevent.extension;
+        events.push_back(fevent);*/
+    } 
+
+    printf("msg %s\n", msg);
+}
+
+auto Watcher::closeModified() -> char
+{
+    if (int notclosed = pclose(Watcher::modifiedpipe); notclosed) {
+        printf("FAILED CLOSING the pipe\n");
+        return 1;
+    }
+    printf("CLOSING the modified pipe\n");
+    return 0;
+}
+
+
 int main()
 {
-    Watcher::pollEvents();
-    auto tevents = Watcher::popEvents("discovered", "textures");
-    auto mevents = Watcher::popEvents("discovered", "shaders");
-    auto matevents = Watcher::popEvents("discovered", "materials");
-    auto modevents = Watcher::popEvents("discovered", "models");
-    auto sceneevents = Watcher::popEvents("discovered", "scenes");
+//    Watcher::discoverFiles();
 
+    if (auto err = Watcher::openModified(); err) {
+        return 1;
+    }
 
-    std::cin.get();
+    for(;;) 
+    {
+        printf("\n\nPOLLING\n");
+        
+        if(auto err = Watcher::pollModified(); err) {
+            return 1;
+        }
+        printf("...polling after\n");
+        using namespace std::chrono_literals;
+        //std::this_thread::sleep_for(1s);
+        std::cin.get();
+    }
+
+    Watcher::closeModified();
+
     return 0;
 }
